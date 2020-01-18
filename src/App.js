cd68a560cb01aaba0e1c06dba94cfcec24e41b92
Mohd Hussain <samsteve124@gmail.com>
2020-01-18 10:19:18 +0530

    update #2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { PureComponent } from 'react'
 import './App.css'
 
+const list = [
+    {
+        title:'First To DO list',
+        completed: false,
+        id:0
+    },
+    {
+        title:'Second To DO list',
+        completed: false,
+        id:1
+    }
+]
+
 class App extends PureComponent {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            list,
+        }
+    }   
     render() {
         return (
             <main>
                 <div>
-                    I'm a block-level-ish element of an unknown width and height, centered vertically within my parent.
+                    {this.state.list.map(item => 
+                        <div key={item.id} className="listDiv">
+                            <div style={{display:'inline-block',width:'80%'}}>
+                                <span>{item.title} </span>
+                            </div>
+                            <span>
+                                <button onClick={this.onClickMe} type="button">Complete</button>
+                            </span>
+                        </div>
+                        )}
                 </div>
             </main>
         )
