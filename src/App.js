68fad459cd303f84bda23b143fc8b3d363422b8e
Mohd Hussain <samsteve124@gmail.com>
2020-01-18 11:05:03 +0530

    search added update #4

--- src/App.js
+++ src/App.js
@@ -11,28 +11,47 @@ const list = [
         title:'Second To DO list',
         completed: false,
         id:1
+    }, 
+    {
+        title: 'Fifth To DO list',
+        completed: false,
+        id: 2
     }
 ]
+
+const isSearched = searchTerm =>  item =>  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase())
 
 class App extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
             list,
+            searchTerm:''
         }
         this.onClickMe = this.onClickMe.bind(this)
+        this.onSearchChange = this.onSearchChange.bind(this)
     } 
     
     onClickMe(id) {
         const updatedList = this.state.list.filter(item => item.id !== id)
         this.setState({list:updatedList})
     }
+
+    onSearchChange(e) {
+        this.setState({searchTerm: e.target.value})
+    }
     
     render() {
         return (
             <main>
                 <div>
-                    {this.state.list.map(item => 
+                <form>
+                    <input 
+                        type="text" 
+                        placeholder="Search Your Todo"
+                        onChange={this.onSearchChange}/>
+                </form>
+                    {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
                         <div key={item.id} className="listDiv">
                             <div style={{display:'inline-block',width:'80%'}}>
                                 <span>{item.title} </span>
